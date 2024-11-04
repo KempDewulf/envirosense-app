31083f7038688846a2d3c9fb07091dd06a14e514
Layton Berth <laytonbrth@gmail.com>
2024-11-04 16:40:03 +0100

    add colors.dart file

---
+++ lib/colors/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF2A2A37);
+  static const Color secondaryColor = Color(0xFFFF9900);
+  static const Color accentColor = Color(0xFF8B8B8B);
+  static const Color redColor = Color(0xFFFE4C4C);
+  static const Color greenColor = Color(0xFF80BC64);
+  static const Color blueColor = Color(0xFF307AFF);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color blackColor = Color(0xFF000000);
+}
