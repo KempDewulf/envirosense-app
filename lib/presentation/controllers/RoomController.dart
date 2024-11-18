685ac2e3f57d92c9e6ebf2e7fea7f70acd9f545c
Layton Berth <laytonbrth@gmail.com>
2024-11-18 20:58:59 +0100

    feat: enhance RoomController with add and remove room use cases

--- lib/presentation/controllers/RoomController.dart
+++ lib/presentation/controllers/RoomController.dart
@@ -1,18 +1,35 @@
 import 'package:envirosense/data/datasources/room_data_source.dart';
 import 'package:envirosense/data/repositories/room_repository_impl.dart';
 import 'package:envirosense/domain/entities/room.dart';
+import 'package:envirosense/domain/repositories/room_repository.dart';
+import 'package:envirosense/domain/usecases/add_room.dart';
+import 'package:envirosense/domain/usecases/remove_room.dart';
 import '../../domain/usecases/get_rooms.dart';
 
 class RoomController {
-  final GetRoomsUseCase getRoomsUseCase;
+  late final GetRoomsUseCase getRoomsUseCase;
+  late final AddRoomUseCase addRoomUseCase;
+  late final RemoveRoomUseCase removeRoomUseCase;
+  final RoomRepository repository;
 
-  RoomController(): getRoomsUseCase = GetRoomsUseCase(
-    RoomRepositoryImpl(
-      remoteDataSource: RoomDataSource(),
-    ),
-  );
+  RoomController()
+      : repository = RoomRepositoryImpl(
+          remoteDataSource: RoomDataSource(),
+        ) {
+    getRoomsUseCase = GetRoomsUseCase(repository);
+    addRoomUseCase = AddRoomUseCase(repository);
+    removeRoomUseCase = RemoveRoomUseCase(repository);
+  }
 
   Future<List<Room>> fetchRooms() async {
     return await getRoomsUseCase();
+  }
+
+  Future<void> addRoom(Room room) async {
+    await addRoomUseCase(room);
+  }
+
+  Future<void> removeRoom(String roomName) async {
+    await removeRoomUseCase(roomName);
   }
 }
