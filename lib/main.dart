972b6a9b9887734bba937022630f42cf189bdd97
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-12-17 22:44:19 +0100

    feat: add flutter_dotenv package for environment variable management

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:envirosense/presentation/views/onboarding_screen.dart';
 import 'package:envirosense/presentation/views/statistics_detail_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -17,6 +18,7 @@ import 'presentation/views/room_overview_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
