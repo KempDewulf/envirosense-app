f12bc6f2cdc58000aee463c63641c208ff9a1c04
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-12-18 00:06:56 +0100

    feat: add methods for environmental scores and room device management in RoomOverviewController

--- lib/presentation/controllers/RoomOverviewController.dart
+++ lib/presentation/controllers/RoomOverviewController.dart
@@ -1,3 +1,5 @@
+import 'package:envirosense/domain/entities/device.dart';
+
 class RoomOverviewController {
   Map<String, Map<String, String>> getRoomData(String roomName) {
     //TODO: implement API call
@@ -15,5 +17,34 @@ class RoomOverviewController {
       'Humidity': {'value': '55%', 'status': 'good'},
       'Air Quality': {'value': '400 ppm', 'status': 'bad'},
     };
+  }
+  //get enviroscore
+  Map<String, Map<String, String>> getEnviroScore() {
+    return {
+      'EnviroScore': {'value': '75', 'status': 'good'},
+    };
+  }
+
+  Map<String, List<double>> getChartData() {
+    // TODO: Implement API call to get historical data
+    return {
+      'temperature': [22, 23, 21, 24, 22],
+      'humidity': [45, 47, 44, 46, 45],
+      'airQuality': [350, 360, 340, 355, 350],
+    };
+  }
+
+  Future<List<Device>> getRoomDevices(String roomId) async {
+    // TODO: Implement API call to get devices in room
+    return [];
+  }
+
+  double getTargetTemperature() {
+    // TODO: Implement API call to get target temperature
+    return 22.0;
+  }
+
+  Future<void> setTargetTemperature(double temperature) async {
+    // TODO: Implement API call to set target temperature
   }
 }
