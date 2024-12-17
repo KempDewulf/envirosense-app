e1810c3e3edf1fc6840f2bac7c940408b96daf02
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-12-17 22:51:27 +0100

    feat: integrate dotenv for API base URL and token management in ApiService

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,11 +1,21 @@
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:convert';
 
 class ApiService {
-  final String baseUrl = 'http://94.130.75.173:8101';
+  final String baseUrl = dotenv.env['API_BASE_URL']!;
+  final String? _token = dotenv.env['API_TOKEN'];
+
+  Map<String, String> get _headers => {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer $_token',
+  };
 
   Future<dynamic> getRequest(String endpoint) async {
-    final response = await http.get(Uri.parse('$baseUrl/$endpoint'));
+    final response = await http.get(
+      Uri.parse('$baseUrl/$endpoint'),
+      headers: _headers,
+    );
 
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
@@ -17,7 +27,7 @@ class ApiService {
   Future<dynamic> postRequest(String endpoint, {Map<String, dynamic>? body}) async {
     final response = await http.post(
       Uri.parse('$baseUrl/$endpoint'),
-      headers: {'Content-Type': 'application/json'},
+      headers: _headers,
       body: body != null ? jsonEncode(body) : null,
     );
 
