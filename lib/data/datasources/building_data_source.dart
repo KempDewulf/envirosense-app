cafe495e2f0e79ea7f78ac1412f4d664b93df8cb
Layton Berth <laytonbrth@gmail.com>
2024-12-19 02:12:41 +0100

    feat: add BuildingDataSource for fetching building data from API

---
+++ lib/data/datasources/building_data_source.dart
@@ -0,0 +1,25 @@
+import 'package:envirosense/data/models/building_model.dart';
+
+import '../../services/api_service.dart';
+
+class BuildingDataSource {
+  final ApiService apiService;
+
+  BuildingDataSource({required this.apiService});
+
+  Future<List<BuildingModel>> getBuildings() async {
+    try {
+      final response = await apiService.getRequest('buildings');
+      List<dynamic> data = response as List<dynamic>;
+      List<BuildingModel> buildings = data.map((buildingJson) {
+          return BuildingModel.fromJson(
+            buildingJson as Map<String, dynamic>,
+        );
+      }).toList();
+      return buildings;
+    } catch (e) {
+      // Handle errors
+      throw Exception('Failed to load buildings: $e');
+    }
+  }
+}
