5fd68ead8e649580b4ead244a98dd2d20ed4ccda
Layton Berth <laytonbrth@gmail.com>
2024-12-19 15:45:21 +0100

    feat: implement addRoom functionality in RoomController

--- lib/presentation/controllers/room_controller.dart
+++ lib/presentation/controllers/room_controller.dart
@@ -3,10 +3,12 @@ import 'package:envirosense/data/repositories/room_repository_impl.dart';
 import 'package:envirosense/domain/entities/room.dart';
 import 'package:envirosense/domain/repositories/room_repository.dart';
 import '../../domain/usecases/get_rooms.dart';
+import '../../domain/usecases/add_room.dart';
 import '../../services/api_service.dart';
 
 class RoomController {
   late final GetRoomsUseCase getRoomsUseCase;
+  late final AddRoomUseCase addRoomUseCase;
   final RoomRepository repository;
 
   RoomController()
@@ -14,9 +16,14 @@ class RoomController {
           remoteDataSource: RoomDataSource(apiService: ApiService()),
         ) {
     getRoomsUseCase = GetRoomsUseCase(repository);
+    addRoomUseCase = AddRoomUseCase(repository);
   }
 
   Future<List<Room>> fetchRooms() async {
     return await getRoomsUseCase();
+  }
+
+  Future<void> addRoom(Room room) async {
+    return await addRoomUseCase(room);
   }
 }
