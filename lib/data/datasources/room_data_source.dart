f4c3d1062f64fcb97b3c9df75777037d8ab20ecc
Layton Berth <laytonbrth@gmail.com>
2024-12-19 15:59:41 +0100

    feat: add addRoom method to RoomDataSource for room creation

--- lib/data/datasources/room_data_source.dart
+++ lib/data/datasources/room_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:envirosense/data/models/add_room_request_model.dart';
+
 import '../../services/api_service.dart';
 import '../models/room_model.dart';
 
@@ -19,6 +21,17 @@ class RoomDataSource {
     } catch (e) {
       // Handle errors
       throw Exception('Failed to load rooms: $e');
+    }
+  }
+
+  Future<void> addRoom(name, buildingId, roomTypeId) async {
+    try {
+      AddRoomRequest body = AddRoomRequest(name, roomTypeId, buildingId);
+
+      await apiService.postRequest('rooms', body.toJson());
+    } catch (e) {
+      // Handle errors
+      throw Exception('Failed to add room: $e');
     }
   }
 }
