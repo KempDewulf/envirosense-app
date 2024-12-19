5a65e56504ea39e3f00a43acad3882e46598a12c
Layton Berth <laytonbrth@gmail.com>
2024-12-19 17:52:10 +0100

    feat: add functionality to assign devices to rooms with new use case and controller integration

--- lib/presentation/controllers/room_controller.dart
+++ lib/presentation/controllers/room_controller.dart
@@ -2,6 +2,7 @@ import 'package:envirosense/data/datasources/room_data_source.dart';
 import 'package:envirosense/data/repositories/room_repository_impl.dart';
 import 'package:envirosense/domain/entities/room.dart';
 import 'package:envirosense/domain/repositories/room_repository.dart';
+import 'package:envirosense/domain/usecases/add_device_to_room.dart';
 import '../../domain/usecases/get_rooms.dart';
 import '../../domain/usecases/add_room.dart';
 import '../../services/api_service.dart';
@@ -9,6 +10,7 @@ import '../../services/api_service.dart';
 class RoomController {
   late final GetRoomsUseCase getRoomsUseCase;
   late final AddRoomUseCase addRoomUseCase;
+  late final AddDeviceToRoomUseCase addDeviceToRoomUseCase;
   final RoomRepository repository;
 
   RoomController()
@@ -17,6 +19,7 @@ class RoomController {
         ) {
     getRoomsUseCase = GetRoomsUseCase(repository);
     addRoomUseCase = AddRoomUseCase(repository);
+    addDeviceToRoomUseCase = AddDeviceToRoomUseCase(repository);
   }
 
   Future<List<Room>> getRooms() async {
@@ -24,7 +27,11 @@ class RoomController {
   }
 
   Future<void> addRoom(
-    String? name, String buildingId, String? roomTypeId) async {
+      String? name, String buildingId, String? roomTypeId) async {
     return await addRoomUseCase(name, buildingId, roomTypeId);
+  }
+
+  Future<void> addDeviceToRoom(String? roomId, String deviceId) async {
+    return await addDeviceToRoomUseCase(roomId, deviceId);
   }
 }
