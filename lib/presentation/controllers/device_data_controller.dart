8161e2929dc3523ed6be15db1dd02abbb06f8103
Layton Berth <laytonbrth@gmail.com>
2024-12-19 02:31:09 +0100

    feat: add DeviceDataController for managing device data retrieval

---
+++ lib/presentation/controllers/device_data_controller.dart
@@ -0,0 +1,23 @@
+import 'package:envirosense/data/datasources/device_data_data_source.dart';
+import 'package:envirosense/data/repositories/device_data_repository_impl.dart';
+import 'package:envirosense/domain/entities/device_data.dart';
+import 'package:envirosense/domain/repositories/device_data_repository.dart';
+import 'package:envirosense/domain/usecases/get_device_data.dart';
+
+import '../../services/api_service.dart';
+
+class DeviceDataController {
+  late final GetDeviceDataUseCase getDeviceDataUseCase;
+  final DeviceDataRepository repository;
+
+  DeviceDataController()
+      : repository = DeviceDataRepositoryImpl(
+          remoteDataSource: DeviceDataDataSource(apiService: ApiService()),
+        ) {
+    getDeviceDataUseCase = GetDeviceDataUseCase(repository);
+  }
+
+  Future<List<DeviceData>> fetchDeviceData() async {
+    return await getDeviceDataUseCase();
+  }
+}
