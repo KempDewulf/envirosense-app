a72f1de8563e82080249f6626d79f6ff3b9d81cb
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-12-20 00:47:07 +0100

    feat: add getRoom method to RoomController for retrieving room details by ID

--- lib/presentation/controllers/room_controller.dart
+++ lib/presentation/controllers/room_controller.dart
@@ -3,12 +3,14 @@ import 'package:envirosense/data/repositories/room_repository_impl.dart';
 import 'package:envirosense/domain/entities/room.dart';
 import 'package:envirosense/domain/repositories/room_repository.dart';
 import 'package:envirosense/domain/usecases/add_device_to_room.dart';
+import 'package:envirosense/domain/usecases/get_room.dart';
 import '../../domain/usecases/get_rooms.dart';
 import '../../domain/usecases/add_room.dart';
 import '../../services/api_service.dart';
 
 class RoomController {
   late final GetRoomsUseCase getRoomsUseCase;
+  late final GetRoomUseCase getRoomUseCase;
   late final AddRoomUseCase addRoomUseCase;
   late final AddDeviceToRoomUseCase addDeviceToRoomUseCase;
   final RoomRepository repository;
@@ -18,12 +20,17 @@ class RoomController {
           remoteDataSource: RoomDataSource(apiService: ApiService()),
         ) {
     getRoomsUseCase = GetRoomsUseCase(repository);
+    getRoomUseCase = GetRoomUseCase(repository);
     addRoomUseCase = AddRoomUseCase(repository);
     addDeviceToRoomUseCase = AddDeviceToRoomUseCase(repository);
   }
 
   Future<List<Room>> getRooms() async {
     return await getRoomsUseCase();
+  }
+
+  Future<Room> getRoom(String roomId) async {
+    return getRoomUseCase(roomId);
   }
 
   Future<void> addRoom(
