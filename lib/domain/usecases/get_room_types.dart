13d933787ef02323fef314ddbc27258ce845ff7f
Layton Berth <laytonbrth@gmail.com>
2024-12-19 01:54:42 +0100

    feat: add GetRoomTypesUseCase for retrieving room types from repository

---
+++ lib/domain/usecases/get_room_types.dart
@@ -0,0 +1,12 @@
+import 'package:envirosense/domain/repositories/room_type_repository.dart';
+import '../entities/RoomType.dart';
+
+class GetRoomTypesUseCase {
+  final RoomTypeRepository repository;
+
+  GetRoomTypesUseCase(this.repository);
+
+  Future<List<RoomType>> call() async {
+    return await repository.getRoomTypes();
+  }
+}
