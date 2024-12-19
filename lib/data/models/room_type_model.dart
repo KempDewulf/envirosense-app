1b3b1948b1f6a10b377b13ddb929f1ad9f4bb685
Layton Berth <laytonbrth@gmail.com>
2024-12-19 16:57:32 +0100

    feat: enhance RoomTypeModel JSON parsing to improve room data handling

--- lib/data/models/room_type_model.dart
+++ lib/data/models/room_type_model.dart
@@ -12,13 +12,23 @@ class RoomTypeModel extends RoomType {
 
   factory RoomTypeModel.fromJson(Map<String, dynamic> json, String buildingId) {
     List<Room> rooms = (json['rooms'] as List)
-        .map((roomJson) => Room(id: roomJson['rooms']['documentId'], name: roomJson['rooms']['name'], roomType: RoomType(id: json['documentId'], name: json['name'], icon: json['name'].toString().toLowerCase()), building: Building(id: buildingId, name: "Campus Brugge Station - Building A", address: "Spoorwegstraat 4, 8200 Brugge")))
+        .map((roomJson) => Room(
+            id: roomJson['rooms']['documentId'],
+            name: roomJson['rooms']['name'],
+            roomType: RoomType(
+                id: json['documentId'],
+                name: json['name'],
+                icon: json['name'].toString().toLowerCase()),
+            building: Building(
+                id: buildingId,
+                name: "Campus Brugge Station - Building A",
+                address: "Spoorwegstraat 4, 8200 Brugge")))
         .toList();
 
     return RoomTypeModel(
       id: json['documentId'],
       name: json['name'],
-      icon: json['icon'],
+      icon: json['name'].toString().toLowerCase(),
       rooms: rooms,
     );
   }
