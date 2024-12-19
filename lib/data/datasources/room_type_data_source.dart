913f548160bffa9ca6542bcfff5dc7b1c70d1e1e
Layton Berth <laytonbrth@gmail.com>
2024-12-19 02:11:33 +0100

    feat: add RoomTypeDataSource for fetching room types from API

---
+++ lib/data/datasources/room_type_data_source.dart
@@ -0,0 +1,24 @@
+import 'package:envirosense/data/models/room_type_model.dart';
+import 'package:envirosense/services/api_service.dart';
+
+class RoomTypeDataSource {
+  final ApiService apiService;
+
+  RoomTypeDataSource({required this.apiService});
+
+  Future<List<RoomTypeModel>> getRoomTypes() async {
+    try {
+      final response = await apiService.getRequest('room-types');
+      List<dynamic> data = response as List<dynamic>;
+      List<RoomTypeModel> roomTypes = data.map((roomTypeJson) {
+          return RoomTypeModel.fromJson(
+            roomTypeJson as Map<String, dynamic>,
+          );
+      }).toList();
+      return roomTypes;
+    } catch (e) {
+      // Handle errors
+      throw Exception('Failed to load room types: $e');
+    }
+  }
+}
