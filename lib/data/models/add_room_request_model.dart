2cf5349d19931526a79c8327f233393e3f6f0647
Layton Berth <laytonbrth@gmail.com>
2024-12-19 15:59:39 +0100

    feat: add AddRoomRequest model for room creation requests

---
+++ lib/data/models/add_room_request_model.dart
@@ -0,0 +1,19 @@
+class AddRoomRequest {
+  final String name;
+  final String buildingId;
+  final String roomTypeId;
+
+  AddRoomRequest(
+    this.name,
+    this.buildingId,
+    this.roomTypeId,
+  );
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'buildingDocumentId': buildingId,
+      'roomTypeDocumentId': roomTypeId,
+    };
+  }
+}
