a0d3271177d6a478c59351885299b5b51323dba7
Layton Berth <laytonbrth@gmail.com>
2024-12-20 04:36:31 +0100

    fix: optimize navigation in SettingsScreen by using a local navigator reference

--- lib/presentation/views/settings_screen.dart
+++ lib/presentation/views/settings_screen.dart
@@ -1,5 +1,3 @@
-// In settings_screen.dart or the relevant file
-
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -8,6 +6,9 @@ class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   Future<void> _signOut(BuildContext context) async {
+    // Get a local reference to check widget state
+    final navigator = Navigator.of(context);
+
     // Clear the login timestamp
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.remove('loginTimestamp');
@@ -15,8 +16,8 @@ class SettingsScreen extends StatelessWidget {
     // Sign out from Firebase
     await FirebaseAuth.instance.signOut();
 
-    // Navigate to login screen
-    Navigator.pushReplacementNamed(context, '/login');
+    // Use the local navigator reference for navigation
+    navigator.pushReplacementNamed('/login');
   }
 
   @override
