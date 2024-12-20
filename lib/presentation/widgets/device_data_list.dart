fdeee473e6b81f0cd151737c81b2daead4bf89d4
Layton Berth <laytonbrth@gmail.com>
2024-12-20 17:56:12 +0100

    feat: refactor device data list to utilize DataStatusHelper for status calculations

--- lib/presentation/widgets/device_data_list.dart
+++ lib/presentation/widgets/device_data_list.dart
@@ -2,60 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../../../core/constants/colors.dart';
 import '../../../domain/entities/device_data.dart';
+import '../../../core/helpers/data_status_helper.dart';
 
 class DeviceDataList extends StatelessWidget {
   final List<DeviceData> deviceData;
 
   const DeviceDataList({super.key, required this.deviceData});
-
-  Status _getStatus(
-    num value, {
-    required num min,
-    required num max,
-    required num optimalMin,
-    required num optimalMax,
-  }) {
-    if (value < min || value > max) return Status.bad;
-    if (value < optimalMin || value > optimalMax) return Status.medium;
-    return Status.good;
-  }
-
-  Status _getTemperatureStatus(double temp) {
-    return _getStatus(
-      temp,
-      min: Thresholds.temperature.min,
-      max: Thresholds.temperature.max,
-      optimalMin: Thresholds.temperature.optimalMin,
-      optimalMax: Thresholds.temperature.optimalMax,
-    );
-  }
-
-  Status _getHumidityStatus(double humidity) {
-    return _getStatus(
-      humidity,
-      min: Thresholds.humidity.min,
-      max: Thresholds.humidity.max,
-      optimalMin: Thresholds.humidity.optimalMin,
-      optimalMax: Thresholds.humidity.optimalMax,
-    );
-  }
-
-  Status _getAirQualityStatus(int ppm) {
-    if (ppm > Thresholds.airQuality.max) return Status.bad;
-    if (ppm > Thresholds.airQuality.optimalMax) return Status.medium;
-    return Status.good;
-  }
-
-  Color _getStatusColor(Status status) {
-    switch (status) {
-      case Status.good:
-        return AppColors.greenColor;
-      case Status.medium:
-        return AppColors.secondaryColor;
-      case Status.bad:
-        return AppColors.redColor;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -110,21 +62,22 @@ class DeviceDataList extends StatelessWidget {
       _buildDataRow(
         label: 'Temperature',
         value: '${data.airData.temperature?.toStringAsFixed(1)}°C',
-        status: _getTemperatureStatus(data.airData.temperature ?? 0),
+        status:
+            DataStatusHelper.getTemperatureStatus(data.airData.temperature ?? 0),
         icon: Icons.thermostat,
       ),
       const SizedBox(height: 12),
       _buildDataRow(
         label: 'Humidity',
         value: '${data.airData.humidity?.toStringAsFixed(1)}%',
-        status: _getHumidityStatus(data.airData.humidity ?? 0),
+        status: DataStatusHelper.getHumidityStatus(data.airData.humidity ?? 0),
         icon: Icons.water_drop,
       ),
       const SizedBox(height: 12),
       _buildDataRow(
         label: 'Air Quality',
         value: '${data.airData.gasPpm} ppm',
-        status: _getAirQualityStatus(data.airData.gasPpm ?? 0),
+        status: DataStatusHelper.getAirQualityStatus(data.airData.gasPpm ?? 0),
         icon: Icons.cloud,
       ),
     ];
@@ -150,38 +103,11 @@ class DeviceDataList extends StatelessWidget {
           width: 12,
           height: 12,
           decoration: BoxDecoration(
-            color: _getStatusColor(status),
+            color: DataStatusHelper.getStatusColor(status),
             shape: BoxShape.circle,
           ),
         ),
       ],
     );
   }
-}
-
-enum Status {
-  good,
-  medium,
-  bad,
-}
-
-class Thresholds {
-  static const temperature = (
-    min: 18.0,
-    max: 35.0,
-    optimalMin: 20.0,
-    optimalMax: 24.0,
-  );
-
-  static const humidity = (
-    min: 30.0,
-    max: 70.0,
-    optimalMin: 40.0,
-    optimalMax: 60.0,
-  );
-
-  static const airQuality = (
-    max: 1000,
-    optimalMax: 800,
-  );
 }
