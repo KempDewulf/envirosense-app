be6f5d2629c609188ee90e6e08731f8d2391ab7a
Layton Berth <laytonbrth@gmail.com>
2024-12-21 03:43:57 +0100

    fix: remove redundant data loading after moving device between rooms

--- lib/presentation/views/device_overview_screen.dart
+++ lib/presentation/views/device_overview_screen.dart
@@ -562,8 +562,6 @@ class _DeviceOverviewScreenState extends State<DeviceOverviewScreen>
       await _roomController.removeDeviceFromRoom(currentRoomId, _device?.id);
       await _roomController.addDeviceToRoom(_selectedRoomId, _device?.id);
 
-      await _loadData();
-
       Navigator.pop(context);
 
       ScaffoldMessenger.of(context).showSnackBar(
