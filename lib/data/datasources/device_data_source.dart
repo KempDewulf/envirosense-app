efb396de4d29edab02a6b93582649111be6b0213
layton <layton.berth@student.howest.be>
2025-01-02 17:05:18 +0100

    refactor: add debug print statements for device deletion process

--- lib/data/datasources/device_data_source.dart
+++ lib/data/datasources/device_data_source.dart
@@ -58,6 +58,7 @@ class DeviceDataSource {
 
   Future<void> deleteDeviceData(String? deviceId) async {
     try {
+      print('devices/$deviceId/device-data');
       await apiService.deleteRequest('devices/$deviceId/device-data');
     } catch (e) {
       throw Exception('Failed to delete device: $e');
--- lib/services/device_service.dart
+++ lib/services/device_service.dart
@@ -34,6 +34,7 @@ class DeviceService {
     if (currentRoomId != null && currentRoomId.isNotEmpty) {
       await removeDeviceFromRoom(currentRoomId, deviceId);
     } else {
+      print("Device is not in any room");
       await _deviceController.deleteDeviceData(deviceId);
     }
 
