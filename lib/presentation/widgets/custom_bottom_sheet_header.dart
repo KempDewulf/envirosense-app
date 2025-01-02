f3e9b6f203e15309dc79cbb994fabd5555ce311e
layton <layton.berth@student.howest.be>
2025-01-02 17:30:51 +0100

    feat: add CustomBottomSheetHeader widget for enhanced bottom sheet presentation

---
+++ lib/presentation/widgets/custom_bottom_sheet_header.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import '../../core/constants/colors.dart';
+
+class CustomBottomSheetHeader extends StatelessWidget {
+  final String title;
+
+  const CustomBottomSheetHeader({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 40,
+          height: 4,
+          margin: const EdgeInsets.symmetric(vertical: 12),
+          decoration: BoxDecoration(
+            color: AppColors.accentColor.withOpacity(0.3),
+            borderRadius: BorderRadius.circular(2),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: AppColors.secondaryColor,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
