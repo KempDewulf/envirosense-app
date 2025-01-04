5b81c87de9f84e90985f378314f6dcaee2b31033
layton <layton.berth@student.howest.be>
2025-01-04 15:17:44 +0100

    feat: add update methods for device UI mode and limits in DeviceController

--- lib/presentation/controllers/device_controller.dart
+++ lib/presentation/controllers/device_controller.dart
@@ -1,8 +1,11 @@
+import 'package:envirosense/core/enums/display_mode.dart';
 import 'package:envirosense/data/datasources/device_data_source.dart';
 import 'package:envirosense/domain/usecases/add_device.dart';
 import 'package:envirosense/domain/usecases/delete_device.dart';
 import 'package:envirosense/domain/usecases/delete_device_data.dart';
 import 'package:envirosense/domain/usecases/get_device.dart';
+import 'package:envirosense/domain/usecases/update_device_limit.dart';
+import 'package:envirosense/domain/usecases/update_device_ui_mode.dart';
 
 import '../../data/repositories/device_repository_impl.dart';
 import '../../domain/entities/device.dart';
@@ -14,6 +17,8 @@ class DeviceController {
   late final GetDevicesUseCase getDevicesUseCase;
   late final GetDeviceUseCase getDeviceUseCase;
   late final AddDeviceUseCase addDeviceUseCase;
+  late final UpdateDeviceUIModeUseCase updateDeviceUIModeUseCase;
+  late final UpdateDeviceLimitUseCase updateDeviceLimitUseCase;
   late final DeleteDeviceUseCase deleteDeviceUseCase;
   late final DeleteDeviceDataUseCase deleteDeviceDataUseCase;
   final DeviceRepository repository;
@@ -25,6 +30,8 @@ class DeviceController {
     getDevicesUseCase = GetDevicesUseCase(repository);
     getDeviceUseCase = GetDeviceUseCase(repository);
     addDeviceUseCase = AddDeviceUseCase(repository);
+    updateDeviceUIModeUseCase = UpdateDeviceUIModeUseCase(repository);
+    updateDeviceLimitUseCase = UpdateDeviceLimitUseCase(repository);
     deleteDeviceUseCase = DeleteDeviceUseCase(repository);
     deleteDeviceDataUseCase = DeleteDeviceDataUseCase(repository);
   }
@@ -39,6 +46,14 @@ class DeviceController {
 
   Future<String> addDevice(String? roomId, String? deviceIdentifier) async {
     return await addDeviceUseCase(roomId, deviceIdentifier);
+  }
+
+  Future<void> updateDeviceUIMode(String deviceId, DisplayMode mode) async {
+    return await updateDeviceUIModeUseCase(deviceId, mode);
+  }
+
+  Future<void> updateDeviceLimit(String deviceId, String limitType, double value) async {
+    return await updateDeviceLimitUseCase(deviceId, limitType, value);
   }
 
   Future<void> deleteDevice(String? deviceId, String? buildingId) async {
