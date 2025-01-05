79817a5b01c2ea7a92eef12144b93841f029a507
layton <layton.berth@student.howest.be>
2025-01-05 16:55:51 +0100

    refactor: add extension method to convert ConfigType to API string representation

--- lib/core/enums/config_type.dart
+++ lib/core/enums/config_type.dart
@@ -2,3 +2,16 @@ enum ConfigType {
   brightness,
   uiMode,
 }
+
+extension ConfigTypeExtension on ConfigType {
+  String get toApiString {
+    switch (this) {
+      case ConfigType.uiMode:
+        return 'ui-mode';
+      case ConfigType.brightness:
+        return 'brightness';
+      default:
+        return name;
+    }
+  }
+}
