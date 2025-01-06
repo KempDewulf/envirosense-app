412b4821adcaed9e04fbd2f3359c1d56b95f9756
Layton Berth <laytonbrth@gmail.com>
2025-01-07 00:58:32 +0100

    feat: add getDeviceConfig method and corresponding use case to DeviceController

--- lib/presentation/controllers/device_controller.dart
+++ lib/presentation/controllers/device_controller.dart
@@ -1,10 +1,12 @@
 import 'package:envirosense/core/enums/config_type.dart';
 import 'package:envirosense/core/enums/limit_type.dart';
 import 'package:envirosense/data/datasources/device_data_source.dart';
+import 'package:envirosense/domain/entities/device_config.dart';
 import 'package:envirosense/domain/usecases/add_device.dart';
 import 'package:envirosense/domain/usecases/delete_device.dart';
 import 'package:envirosense/domain/usecases/delete_device_data.dart';
 import 'package:envirosense/domain/usecases/get_device.dart';
+import 'package:envirosense/domain/usecases/get_device_config.dart';
 import 'package:envirosense/domain/usecases/update_device_config.dart';
 import 'package:envirosense/domain/usecases/update_device_limit.dart';
 import '../../data/repositories/device_repository_impl.dart';
@@ -16,6 +18,7 @@ import '../../services/api_service.dart';
 class DeviceController {
   late final GetDevicesUseCase getDevicesUseCase;
   late final GetDeviceUseCase getDeviceUseCase;
+  late final GetDeviceConfigUseCase getDeviceConfigUseCase;
   late final AddDeviceUseCase addDeviceUseCase;
   late final UpdateDeviceConfigUseCase updateDeviceConfigUseCase;
   late final UpdateDeviceLimitUseCase updateDeviceLimitUseCase;
@@ -29,6 +32,7 @@ class DeviceController {
         ) {
     getDevicesUseCase = GetDevicesUseCase(repository);
     getDeviceUseCase = GetDeviceUseCase(repository);
+    getDeviceConfigUseCase = GetDeviceConfigUseCase(repository);
     addDeviceUseCase = AddDeviceUseCase(repository);
     updateDeviceConfigUseCase = UpdateDeviceConfigUseCase(repository);
     updateDeviceLimitUseCase = UpdateDeviceLimitUseCase(repository);
@@ -42,6 +46,10 @@ class DeviceController {
 
   Future<Device> getDevice(String deviceId, String buildingId) async {
     return await getDeviceUseCase(deviceId, buildingId);
+  }
+
+  Future<DeviceConfig> getDeviceConfig(String deviceId) async {
+    return await getDeviceConfigUseCase(deviceId);
   }
 
   Future<String> addDevice(String? roomId, String? deviceIdentifier) async {
