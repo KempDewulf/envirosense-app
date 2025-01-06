117a8c016994ef18ccbb7ce58d797fb5e225f758
Layton Berth <laytonbrth@gmail.com>
2025-01-07 00:58:47 +0100

    feat: add getDeviceConfig method to DeviceRepository for retrieving device configuration

--- lib/domain/repositories/device_repository.dart
+++ lib/domain/repositories/device_repository.dart
@@ -1,10 +1,12 @@
 import 'package:envirosense/core/enums/config_type.dart';
 import 'package:envirosense/core/enums/limit_type.dart';
+import 'package:envirosense/domain/entities/device_config.dart';
 import '../entities/device.dart';
 
 abstract class DeviceRepository {
   Future<List<Device>> getDevices(String buildingId);
   Future<Device> getDevice(String deviceId, String buildingId);
+  Future<DeviceConfig> getDeviceConfig(String deviceId);
   Future<String> addDevice(String? roomId, String? deviceIdentifier);
   Future<void> deleteDevice(String? deviceId, String? buildingId);
   Future<void> deleteDeviceData(String? deviceId);
