b935c275de5171d96e745100f9065c00a3094379
Layton Berth <laytonbrth@gmail.com>
2025-01-10 00:41:56 +0100

    feat: integrate loading and error handling in add room screen

--- lib/presentation/views/add_room_screen.dart
+++ lib/presentation/views/add_room_screen.dart
@@ -7,6 +7,7 @@ import 'package:envirosense/presentation/controllers/room_type_controller.dart';
 import 'package:envirosense/presentation/widgets/actions/no_connection_widget.dart';
 import 'package:envirosense/presentation/widgets/core/custom_text_form_field.dart';
 import 'package:envirosense/presentation/widgets/feedback/custom_snackbar.dart';
+import 'package:envirosense/presentation/widgets/feedback/loading_error_widget.dart';
 import 'package:flutter/material.dart';
 import '../../core/constants/colors.dart';
 import 'package:envirosense/services/logging_service.dart';
@@ -42,15 +43,6 @@ class _AddRoomScreenState extends State<AddRoomScreen> {
 
     try {
       setState(() => _isLoading = true);
-
-      final connectivityResult = await Connectivity().checkConnectivity();
-      if (connectivityResult == ConnectivityResult.none) {
-        setState(() {
-          _error = 'no_connection';
-          _isLoading = false;
-        });
-        return;
-      }
 
       final roomTypes = await _roomTypesController.getRoomTypes(_buildingId);
 
@@ -143,149 +135,135 @@ class _AddRoomScreenState extends State<AddRoomScreen> {
   }
 
   Widget _buildBody() {
-    if (_isLoading) {
-      return const Center(
-        child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(AppColors.secondaryColor)),
-      );
-    }
-
-    if (_error != null) {
-      return NoConnectionWidget(
-        onRetry: () async {
-          // Clear error state before retrying
-          setState(() {
-            _error = null;
-          });
-          await _loadRoomTypes();
-        },
-      );
-    }
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "Enter Room's name",
-            style: TextStyle(
-              fontSize: 18,
-              color: AppColors.accentColor,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 8),
-          CustomTextFormField(
-            controller: _roomNameController,
-            labelText: 'e.g. Living Room',
-            floatingLabelBehaviour: FloatingLabelBehavior.never,
-            onChanged: (value) => setState(() {}),
-            labelColor: AppColors.accentColor,
-            contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 16),
-            borderColor: AppColors.accentColor,
-            floatingLabelCustomStyle: const TextStyle(
-              color: AppColors.primaryColor,
-            ),
-            textStyle: const TextStyle(
-              color: AppColors.primaryColor,
-            ),
-          ),
-          const SizedBox(height: 28),
-          const Text(
-            "Select Room's icon",
-            style: TextStyle(
-              fontSize: 18,
-              color: AppColors.accentColor,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 20),
-          Expanded(
-            child: GridView.builder(
-              itemCount: _roomTypes?.length ?? 0,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                mainAxisSpacing: 25,
-                crossAxisSpacing: 35,
-                childAspectRatio: 1 / 1.15,
-              ),
-              itemBuilder: (context, index) {
-                final roomType = _roomTypes![index];
-                final isSelected = _selectedRoomType == roomType;
-
-                return GestureDetector(
-                  onTap: () {
-                    FocusScope.of(context).unfocus();
-                    setState(() {
-                      _selectedRoomType = roomType;
-                    });
-                  },
-                  child: Column(
-                    children: [
-                      Container(
-                        width: 70,
-                        height: 70,
-                        decoration: BoxDecoration(
-                          color: isSelected ? AppColors.secondaryColor : AppColors.lightGrayColor,
-                          shape: BoxShape.circle,
-                          boxShadow: isSelected
-                              ? [
-                                  BoxShadow(
-                                    color: AppColors.secondaryColor.withOpacity(0.6),
-                                    spreadRadius: 2,
-                                    blurRadius: 6,
-                                  ),
-                                ]
-                              : [],
-                        ),
-                        child: Icon(
-                          getIconData(roomType.icon),
-                          color: isSelected ? AppColors.whiteColor : AppColors.accentColor,
-                          size: 35,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        capitalizeWords(roomType.icon),
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: isSelected ? AppColors.secondaryColor : AppColors.accentColor,
-                          fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: _isFormComplete ? _saveRoom : null,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: _isFormComplete ? AppColors.secondaryColor : AppColors.lightGrayColor,
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.0),
+    return LoadingErrorWidget(
+        isLoading: _isLoading,
+        error: _error,
+        onRetry: _loadRoomTypes,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Enter Room's name",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: AppColors.accentColor,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              child: _isSaving
-                  ? const CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(AppColors.whiteColor),
-                    )
-                  : const Text(
-                      'SAVE',
-                      style: TextStyle(
-                        color: AppColors.whiteColor,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+              const SizedBox(height: 8),
+              CustomTextFormField(
+                controller: _roomNameController,
+                labelText: 'e.g. Living Room',
+                floatingLabelBehaviour: FloatingLabelBehavior.never,
+                onChanged: (value) => setState(() {}),
+                labelColor: AppColors.accentColor,
+                contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+                borderColor: AppColors.accentColor,
+                floatingLabelCustomStyle: const TextStyle(
+                  color: AppColors.primaryColor,
+                ),
+                textStyle: const TextStyle(
+                  color: AppColors.primaryColor,
+                ),
+              ),
+              const SizedBox(height: 28),
+              const Text(
+                "Select Room's icon",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: AppColors.accentColor,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: _roomTypes?.length ?? 0,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 25,
+                    crossAxisSpacing: 35,
+                    childAspectRatio: 1 / 1.15,
+                  ),
+                  itemBuilder: (context, index) {
+                    final roomType = _roomTypes![index];
+                    final isSelected = _selectedRoomType == roomType;
+
+                    return GestureDetector(
+                      onTap: () {
+                        FocusScope.of(context).unfocus();
+                        setState(() {
+                          _selectedRoomType = roomType;
+                        });
+                      },
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 70,
+                            height: 70,
+                            decoration: BoxDecoration(
+                              color: isSelected ? AppColors.secondaryColor : AppColors.lightGrayColor,
+                              shape: BoxShape.circle,
+                              boxShadow: isSelected
+                                  ? [
+                                      BoxShadow(
+                                        color: AppColors.secondaryColor.withOpacity(0.6),
+                                        spreadRadius: 2,
+                                        blurRadius: 6,
+                                      ),
+                                    ]
+                                  : [],
+                            ),
+                            child: Icon(
+                              getIconData(roomType.icon),
+                              color: isSelected ? AppColors.whiteColor : AppColors.accentColor,
+                              size: 35,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            capitalizeWords(roomType.icon),
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: isSelected ? AppColors.secondaryColor : AppColors.accentColor,
+                              fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+                            ),
+                          ),
+                        ],
                       ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: _isFormComplete ? _saveRoom : null,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: _isFormComplete ? AppColors.secondaryColor : AppColors.lightGrayColor,
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
                     ),
-            ),
+                  ),
+                  child: _isSaving
+                      ? const CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(AppColors.whiteColor),
+                        )
+                      : const Text(
+                          'SAVE',
+                          style: TextStyle(
+                            color: AppColors.whiteColor,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
