80cce2cee8fe2ab6fedc759740bdf4fe63133ac6
Layton Berth <laytonbrth@gmail.com>
2025-01-12 17:53:28 +0100

    refactor: remove unused import from add options bottom sheet

--- lib/presentation/widgets/bottom_sheets/add_options_bottom_sheet.dart
+++ lib/presentation/widgets/bottom_sheets/add_options_bottom_sheet.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:envirosense/core/enums/add_option_type.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
