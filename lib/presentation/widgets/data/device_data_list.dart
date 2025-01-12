8629b2b67e704ebbb949bdfb11b540e6dec77d83
Layton Berth <laytonbrth@gmail.com>
2025-01-12 18:19:00 +0100

    feat: add localization for 'No device data from this device.' message in DeviceDataList widget

--- lib/presentation/widgets/data/device_data_list.dart
+++ lib/presentation/widgets/data/device_data_list.dart
@@ -36,6 +36,7 @@ class _DeviceDataListState extends State<DeviceDataList> {
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
     return RefreshIndicator(
       onRefresh: widget.onRefresh,
       color: AppColors.secondaryColor,
@@ -46,9 +47,9 @@ class _DeviceDataListState extends State<DeviceDataList> {
               padding: const EdgeInsets.all(16),
               children: [
                 if (widget.deviceData.isEmpty)
-                  const Center(
+                  Center(
                     child: Text(
-                      'No device data from this device.',
+                      l10n.noDeviceData,
                       style: TextStyle(
                         fontSize: 16,
                         color: AppColors.accentColor,
